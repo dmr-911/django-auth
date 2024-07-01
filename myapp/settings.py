107ec8f4d777c509f2d6044073b62a50831fa0ca
dmr-911 <dewan.mizanur911@gmail.com>
2024-07-01 22:57:37 +0600

    feat: mysql integrated

--- myapp/settings.py
+++ myapp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mizan',
+        'USER': 'root',
+        'PASSWORD': 'Mizan@123',
+        'HOST': 'localhost',  # Or the address of your MySQL server
+        'PORT': '3306',  # Default MySQL port
     }
 }
 
