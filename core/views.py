10919339a5b161d3cbe08ec14aab18c899f6ddae
dmr-911 <dewan.mizanur911@gmail.com>
2024-07-02 23:09:28 +0600

    feat: created exception and serializers for register user

--- core/views.py
+++ core/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from rest_framework import exceptions
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import UserSerializer
 
 
 # Create your views here.
 class RegisterAPIView(APIView):
     def post(self, request):
+        data = request.data
+        
+        if data['password'] != data['password_confirm']:
+            raise exceptions.APIException('Password do not match')
+        
+        serializer = UserSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(request.data)
